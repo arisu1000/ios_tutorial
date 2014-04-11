94ef4f462da906077d08660f7122cb7fba1ffa56
wcjung <wcjung@daumcorp.com>
2014-04-11 18:31:59 +0900

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by wcjung on 2014. 4. 11..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
